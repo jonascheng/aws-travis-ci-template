9a7cbfa53a1fbf5acad2881fddbd53189dce2889
Jonas Cheng <jonas.cheng@gmail.com>
2016-07-02 21:48:23 +0800

    init travis template

--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo 'Start to build on branch ${1}, commit ${2}, build number ${3}'
+echo 'Start to build on branch ' ${1} '#' ${2} 
+echo 'Travis build number ' ${3} ', my build number ' ${4}
+
 
